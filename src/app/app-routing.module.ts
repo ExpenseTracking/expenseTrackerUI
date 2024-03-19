fc29e3617a684588862f8bf33d8dff2d1615bcc7
MarkCodez <markeskander745@gmail.com>
2024-03-19 12:45:30 -0500

    added necessary routing for different components. making button for switching pages. wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { TransactionTypeComponent } from './transaction-type/transaction-type.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/appHome', pathMatch: 'full' },
+    { path: 'appHome', component: AppComponent },
+    { path: 'transactionType', component: TransactionTypeComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
