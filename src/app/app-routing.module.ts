1fef11eabbbdfe9a462b662f47b1e44e12400c3e
MarkCodez <109993306+MarkCodez@users.noreply.github.com>
2024-03-20 21:56:50 -0500

    Page/home (#1)
    * added necessary routing for different components. making button for switching pages. wip
    
    * just in case commit . wip
    
    * working stuff for web app. changed routing-module to not include 'AppComponent' because duplication. added new component for specific home page. wip
    
    * reorganized files. deleted old files with temp name and added new files with descriptive names for home nav and footer. app component html now renders nav and footer components. wip.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CompanyHomePageComponent } from './Home/company-home-page/company-home-page.component';
+import { CompanyNavbarComponent } from './Home/company-navbar/company-navbar.component';
+import { CompanyFooterComponent } from './Home/company-footer/company-footer.component';
+import { TransactionTypeComponent } from './Home/transaction-type/transaction-type.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/companyHome', pathMatch: 'full' },
+    { path: 'companyHome', component: CompanyHomePageComponent},
+    { path: 'companyNav', component: CompanyNavbarComponent},
+    { path: 'companyFooter', component: CompanyFooterComponent},
+    { path: 'transactionType', component: TransactionTypeComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { TransactionTypeComponent } from './transaction-type/transaction-type.component';
+import { TransactionTypeComponent } from './Home/transaction-type/transaction-type.component';
+import { CompanyHomePageComponent } from './Home/company-home-page/company-home-page.component';
+import { CompanyNavbarComponent } from './Home/company-navbar/company-navbar.component';
+import { CompanyFooterComponent } from './Home/company-footer/company-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TransactionTypeComponent
+    TransactionTypeComponent,
+    CompanyHomePageComponent,
+    CompanyNavbarComponent,
+    CompanyFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
