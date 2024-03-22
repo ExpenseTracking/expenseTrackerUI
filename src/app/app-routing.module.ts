d18a4b8460a2eff814ff6b782a32b752658aa1c6
MarkCodez <109993306+MarkCodez@users.noreply.github.com>
2024-03-21 20:09:13 -0500

    initial commit for dashboard page. lazy layout done. wip (#3)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { CompanyHomePageComponent } from './Home/company-home-page/company-home-
 import { CompanyNavbarComponent } from './Home/company-navbar/company-navbar.component';
 import { CompanyFooterComponent } from './Home/company-footer/company-footer.component';
 import { TransactionTypeComponent } from './Home/transaction-type/transaction-type.component';
+import { ServiceDashboardComponent } from './Home/service-dashboard/service-dashboard.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/companyHome', pathMatch: 'full' },
     { path: 'companyHome', component: CompanyHomePageComponent},
     { path: 'companyNav', component: CompanyNavbarComponent},
     { path: 'companyFooter', component: CompanyFooterComponent},
+    { path: 'dashboard', component: ServiceDashboardComponent},
     { path: 'transactionType', component: TransactionTypeComponent }
   ];
 
