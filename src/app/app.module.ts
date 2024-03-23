66249733c055ae873920e8f14fcbfb0035765e2f
Andrew Preston <74381686+aprest10@users.noreply.github.com>
2024-03-23 17:56:33 -0500

    Add material to project (#4)
    * Add material to project
    
    * edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 import { AppComponent } from './app.component';
 import { TransactionTypeComponent } from './Home/transaction-type/transaction-type.component';
 import { CompanyHomePageComponent } from './Home/company-home-page/company-home-page.component';
 import { CompanyNavbarComponent } from './Home/company-navbar/company-navbar.component';
 import { CompanyFooterComponent } from './Home/company-footer/company-footer.component';
+<<<<<<< HEAD
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+=======
 import { ServiceDashboardComponent } from './Home/service-dashboard/service-dashboard.component';
+>>>>>>> origin/main
 
 @NgModule({
   declarations: [
@@ -25,9 +31,12 @@ import { ServiceDashboardComponent } from './Home/service-dashboard/service-dash
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSlideToggleModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
