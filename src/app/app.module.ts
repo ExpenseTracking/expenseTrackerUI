594c8ecb602c86c137bc9255772f7abb47a87635
MarkCodez <109993306+MarkCodez@users.noreply.github.com>
2024-03-23 18:06:12 -0500

    Revert "Add material to project (#4)"
    This reverts commit 66249733c055ae873920e8f14fcbfb0035765e2f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 import { AppComponent } from './app.component';
 import { TransactionTypeComponent } from './Home/transaction-type/transaction-type.component';
 import { CompanyHomePageComponent } from './Home/company-home-page/company-home-page.component';
 import { CompanyNavbarComponent } from './Home/company-navbar/company-navbar.component';
 import { CompanyFooterComponent } from './Home/company-footer/company-footer.component';
-<<<<<<< HEAD
-
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-=======
 import { ServiceDashboardComponent } from './Home/service-dashboard/service-dashboard.component';
->>>>>>> origin/main
 
 @NgModule({
   declarations: [
@@ -31,12 +25,9 @@ import { ServiceDashboardComponent } from './Home/service-dashboard/service-dash
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    MatSlideToggleModule
+    HttpClientModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
