1c20134acb53e06cc8541a929f40106f49c3b32a
eborel2995 <patsnation0828@gmail.com>
2024-03-23 22:01:43 -0500

    Set up the FAQ page and added some questions with answers. Formatted with inline css styling and added the routing to the nav bar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CompanyNavbarComponent } from './Home/company-navbar/company-navbar.com
 import { CompanyFooterComponent } from './Home/company-footer/company-footer.component';
 import { TransactionTypeComponent } from './Home/transaction-type/transaction-type.component';
 import { ServiceDashboardComponent } from './Home/service-dashboard/service-dashboard.component';
+import { FAQPageComponent } from './Home/faq-page/faq-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/companyHome', pathMatch: 'full' },
@@ -13,7 +14,8 @@ const routes: Routes = [
     { path: 'companyNav', component: CompanyNavbarComponent},
     { path: 'companyFooter', component: CompanyFooterComponent},
     { path: 'dashboard', component: ServiceDashboardComponent},
-    { path: 'transactionType', component: TransactionTypeComponent }
+    { path: 'transactionType', component: TransactionTypeComponent },
+    { path: 'faqs', component: FAQPageComponent}
   ];
 
 @NgModule({
