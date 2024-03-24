5b0cd25c3000ff1ac591fd4fba1f777a81817449
MarkCodez <markeskander745@gmail.com>
2024-03-23 20:48:41 -0500

    expense section with mock data added. new buttons for edit, delete, details, and add new. add new button opens modal for template input form. wip

--- src/app/Home/service-revenue/service-revenue.component.ts
+++ src/app/Home/service-revenue/service-revenue.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
 import { mockIncome } from './mock-income';
-import { NgFor } from '@angular/common';
+import { mockExpense } from './mock-expense';
 
 @Component({
-  selector: 'app-service-revenue',
-  templateUrl: './service-revenue.component.html',
-  styleUrl: './service-revenue.component.css'
+    selector: 'app-service-revenue',
+    templateUrl: './service-revenue.component.html',
+    styleUrl: './service-revenue.component.css'
 })
 export class ServiceRevenueComponent {
+    // dummy data arrays
     income = mockIncome;
+    expense = mockExpense;
+
+    // open/close functions to add a new income
+    openIncomeModal() {
+        const incomeModalDiv = document.getElementById('newIncomeModal');
+        if (incomeModalDiv != null) {
+            incomeModalDiv.style.display = 'block';
+        }
+    }
+
+    closeIncomeModal() {
+        const incomeModalDiv = document.getElementById('newIncomeModal');
+        if (incomeModalDiv != null) {
+            incomeModalDiv.style.display = 'none';
+        }
+    }
+
+    // open/close functions to add a new expense
+    openExpenseModal() {
+        const incomeModalDiv = document.getElementById('newExpenseModal');
+        if (incomeModalDiv != null) {
+            incomeModalDiv.style.display = 'block';
+        }
+    }
+
+    closeExpenseModal() {
+        const incomeModalDiv = document.getElementById('newExpenseModal');
+        if (incomeModalDiv != null) {
+            incomeModalDiv.style.display = 'none';
+        }
+    }
 }
