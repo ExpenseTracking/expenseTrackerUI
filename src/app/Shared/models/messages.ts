240a87be28dc31c193e7bf0dfccd41d6ac12c7d0
aprest10 <aprest10@icloud.com>
2024-03-25 21:11:24 -0500

    add table

---
+++ src/app/Shared/models/messages.ts
@@ -0,0 +1,12 @@
+export const messages = {
+    NEW_TRANSACTION_TYPE: "New TransactionType created",
+    EDIT_TRANSACTION_TYPE: "Edited TransactionType",
+    DELETE_TRANSACTION_TYPE: "Deleted TransactionType",
+
+    info: {
+        success: "Operation completed successfully."
+    },
+    error: {
+        failure: "An error occured."
+    }
+}
---
+++ src/enviornments/enviornment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    envName: 'localhost',
+    api_url: 'https://localhost:7121/api',
+    api_key: ''
+}
