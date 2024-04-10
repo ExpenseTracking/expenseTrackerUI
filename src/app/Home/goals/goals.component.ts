ba83696118aa59f31f55f8a71e77d1c2e9f239d7
eborel2995 <patsnation0828@gmail.com>
2024-04-09 21:58:18 -0500

    Set up goals page

--- src/app/Home/goals/goals.component.ts
+++ src/app/Home/goals/goals.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
+
+import { Goal } from '../../Shared/models/goal';
+import { ApiService } from '../../Shared/app.service';
+import { DeleteGoalDialogComponent } from './delete-goal-dialog/delete-goal-dialog.component';
+import { EditGoalDialogComponent } from './edit-goal-dialog/edit-goal-dialog.component';
+import { CreateGoalDialogComponent } from './create-goal-dialog/create-goal-dialog.component';
+
 
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.component.html',
   styleUrl: './goals.component.css'
 })
-export class GoalsComponent {
+export class GoalsComponent implements OnInit {
+  // columns for goal data
+  goalColumns: string[] = ['description', 'date', 'deadline', 'completed', 'buttons'];
 
+  //data variable to hold goals
+  goal: Goal[] = [];
+
+  constructor(private apiService: ApiService,
+    private goalDialog: MatDialog,
+    private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    this.apiService.getGoalsById(3).subscribe((data: Goal[]) => {
+      this.goal = data;
+    })
+  }
+
+  deleteGoalRow(goalId: any) {
+    const dialogRef = this.goalDialog.open(DeleteGoalDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'delete') {
+        console.log(result);
+        this.apiService.deleteGoal(goalId).subscribe(() => {
+          this.apiService.getGoalsById(3).subscribe(updatedGoal => {
+            this.goal = updatedGoal;
+            this.cdr.detectChanges();
+          });
+        });
+      }
+    });
+  }
+
+  editGoal(row: any) {
+    const dialogRef = this.goalDialog.open(EditGoalDialogComponent, {
+      data: {row}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.action === 'updateGoal') {
+        this.apiService.updateGoal(result.goalId, result).subscribe(() => {
+          this.apiService.getGoalsById(3).subscribe(updatedGoal => {
+            this.goal = updatedGoal;
+            this.cdr.detectChanges();
+          });
+        });
+      }
+    });
+  }
+
+  createGoal() {
+    const dialogRef = this.goalDialog.open(CreateGoalDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result.action === 'createGoal') {
+        this.apiService.createGoal(result).subscribe(() => {
+          this.apiService.getGoalsById(3).subscribe(updatedGoal => {
+            this.goal = updatedGoal;
+            this.cdr.detectChanges();
+          });
+        });
+      }
+    });
+  }
 }
