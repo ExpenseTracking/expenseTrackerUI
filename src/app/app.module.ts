c5e209decb6f86775ad867c183eab1bc5a35e1a7
MarkCodez <markeskander745@gmail.com>
2024-04-12 19:23:50 -0500

    file formatting. no code changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { DatePipe } from '@angular/common';
 import { MatIconButton } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card'; 
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
- import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { TransactionTypeComponent } from './Home/transaction-type/transaction-type.component';
 import { CompanyHomePageComponent } from './Home/company-home-page/company-home-page.component';
 import { CompanyNavbarComponent } from './Home/company-navbar/company-navbar.component';
@@ -44,53 +44,53 @@ import { EditGoalDialogComponent } from './Home/goals/edit-goal-dialog/edit-goal
 import { CreateGoalDialogComponent } from './Home/goals/create-goal-dialog/create-goal-dialog.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TransactionTypeComponent,
-    CompanyHomePageComponent,
-    CompanyNavbarComponent,
-    CompanyFooterComponent,
-    CompanyReviewsComponent,
-    ServiceDashboardComponent,
-    ServiceRevenueComponent,
-    AboutPageComponent,
-    FAQPageComponent,
-    DeleteRevenueDialogComponent,
-    ProfilePageComponent,
-    IncomeComponent,
-    ExpenseComponent,
-    AddIncomeDialogComponent,
-    AddExpenseDialogComponent,
-    EditIncomeDialogComponent,
-    EditExpenseDialogComponent,
-    GoalsComponent,
-    DeleteGoalDialogComponent,
-    EditGoalDialogComponent,
-    CreateGoalDialogComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    MatMenuModule,
-    MatCheckboxModule,
-    MatButtonModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDialogModule,
-    MatTableModule,
-    MatGridListModule,
-    MatIconButton,
-    MatDividerModule,
-    MatListModule,
-    MatCardModule
-  ],
-  providers: [
-    provideAnimationsAsync(),
-    DatePipe
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TransactionTypeComponent,
+        CompanyHomePageComponent,
+        CompanyNavbarComponent,
+        CompanyFooterComponent,
+        CompanyReviewsComponent,
+        ServiceDashboardComponent,
+        ServiceRevenueComponent,
+        AboutPageComponent,
+        FAQPageComponent,
+        DeleteRevenueDialogComponent,
+        ProfilePageComponent,
+        IncomeComponent,
+        ExpenseComponent,
+        AddIncomeDialogComponent,
+        AddExpenseDialogComponent,
+        EditIncomeDialogComponent,
+        EditExpenseDialogComponent,
+        GoalsComponent,
+        DeleteGoalDialogComponent,
+        EditGoalDialogComponent,
+        CreateGoalDialogComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        MatMenuModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDialogModule,
+        MatTableModule,
+        MatGridListModule,
+        MatIconButton,
+        MatDividerModule,
+        MatListModule,
+        MatCardModule
+    ],
+    providers: [
+        provideAnimationsAsync(),
+        DatePipe
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
