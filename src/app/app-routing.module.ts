9cf0ce4cbff14575db751083aa9beca34b92533a
layvong <145372097+layvong@users.noreply.github.com>
2024-04-17 19:57:17 -0500

    Feat/retirement (#33)
    * new retirement calculator page started. modified navbar and footer. wip.
    
    * basic retirement calculator structure, may need more inputs for the actual calculation. the implementation of retirement function is wip.
    
    * adjusted customer testimonies in the reviews page and finished the retirement calculator feature
    
    * added authguard for retirement in routes
    
    * changed styling for calculator.
    
    ---------
    
    Co-authored-by: MarkCodez <markeskander745@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { ServiceRevenueComponent } from './Home/service-revenue/service-revenue.
 import { AboutPageComponent } from './Home/about-page/about-page.component';
 import { FAQPageComponent } from './Home/faq-page/faq-page.component';
 import { DeleteRevenueDialogComponent } from './Home/service-revenue/delete-revenue-dialog/delete-revenue-dialog.component';
+import { RetirementCalculatorComponent } from './Home/retirement-calculator/retirement-calculator.component';
 import { ProfilePageComponent } from './Home/profile-page/profile-page.component';
 import { GoalsComponent } from './Home/goals/goals.component';
 import { LoginComponent } from './Home/login/login.component';
@@ -31,6 +32,8 @@ const routes: Routes = [
     { path: 'profile', component: ProfilePageComponent, canActivate: [AuthGuard]},
     { path: 'about', component: AboutPageComponent},
     { path: 'faqs', component: FAQPageComponent},
+    { path: 'deleteRevenue', component: DeleteRevenueDialogComponent},
+    { path: 'retirement', component: RetirementCalculatorComponent, canActivate: [AuthGuard]},
     { path: 'deleteRevenue', component: DeleteRevenueDialogComponent, canActivate: [AuthGuard]},
     { path: 'goals', component: GoalsComponent, canActivate: [AuthGuard]}
   ];
